7c2b3af2f8479e461b6aee7a7ffebd1296582922
Vlad Solovey <connectwithvlad@gmail.com>
2023-01-20 19:53:53 +0300

    feat: add place mark on field functionality

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const events = {
+  events: {},
+  on(eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+  off(eventName, fn) {
+    if (this.events[eventName]) {
+      for (let i = 0; i < this.events[eventName].length; i++) {
+        if (this.events[eventName][i] === fn) {
+          this.events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  },
+  emit(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((fn) => {
+        fn(data);
+      });
+    }
+  },
+};
+
 const Field = (rowIndex, columnIndex, fieldValue = null) => {
   const field = document.createElement('div');
   field.classList.add('field');
@@ -66,5 +91,34 @@ const gameBoard = ((boardSize) => {
     return board.map((row) => [...row]);
   }
 
+  function handleBoardClick(e) {
+    if (!e.target.classList.contains('field') || e.target.getAttribute('data-value') !== 'null') {
+      return;
+    }
+
+    const field = e.target;
+    const rowIndex = field.getAttribute('data-row');
+    const columnIndex = field.getAttribute('data-column');
+
+    events.emit('fieldClicked', { rowIndex, columnIndex });
+  }
+
+  boardDOM.addEventListener('click', handleBoardClick);
+
   return { updateFieldValue, clearBoard, getBoard };
 })(3);
+
+const gameController = ((board) => {
+  let nextMove = 'cross';
+
+  function handleFieldClicked(data) {
+    board.updateFieldValue(data.rowIndex, data.columnIndex, nextMove);
+    changeNextMove();
+  }
+
+  function changeNextMove() {
+    nextMove = nextMove === 'cross' ? 'circle' : 'cross';
+  }
+
+  events.on('fieldClicked', handleFieldClicked);
+})(gameBoard);
